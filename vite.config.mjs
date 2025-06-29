d8a946059c672e62307248ec226e6c733f5e64bb
carbonsoda <carbonsoda@outlook.com>
2025-06-29 10:32:58 +0800

    workspace configs update

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,13 +3,8 @@ import { defineConfig } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
-	server: {
-		port: 5200,
-	},
-	build: {
-		target: "esnext",
-		outDir: "docs",
-	},
+	server: { port: 5200 },
+	build: { target: "esnext", outDir: "docs" },
 	base: "",
 	plugins: [ViteMinifyPlugin()],
 });
