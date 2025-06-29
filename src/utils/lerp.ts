04d3393756a1dedcf020276873f37a61551c31eb
carbonsoda <carbonsoda@outlook.com>
2025-06-29 12:04:11 +0800

    new lerper

--- src/utils/lerp.ts
+++ src/utils/lerp.ts
@@ -1,14 +1,17 @@
 export function lerp<T>(
-	speed: T,
-	decay: number,
-	threshold: number,
-	callback: (speed: T) => void,
-	transform: (speed: T) => number = (s) => +s,
-	next: (speed: T, decay: number) => T = (s, d) => <T>(+s * d),
-) {
-	if (transform(speed) < threshold) return;
-	callback(speed);
-	requestAnimationFrame(() =>
-		lerp(next(speed, decay), decay, threshold, callback, transform, next),
-	);
+	u: T | undefined,
+	a: number,
+
+	func: (v: T) => void,
+	next: (u: T, a: number) => T | undefined,
+): void {
+	if (u === undefined) {
+		return;
+	}
+
+	func(u);
+
+	requestAnimationFrame(() => {
+		lerp(next(u, a), a, func, next);
+	});
 }
