17a6a50c54445a28bf2da671054d866c0cb754e6
carbonsoda <carbonsoda@outlook.com>
2025-06-29 20:55:54 +0800

    demo env improvements, continue to debug

--- src/index.ts
+++ src/index.ts
@@ -1,186 +1,191 @@
-import { Cvs } from "./engine/canvas";
-import { Scene } from "./engine/scene";
-import { Mat } from "./maths/matrix";
-import { Vec } from "./maths/vector";
-import { Obj } from "./objects/object";
-import { Tri } from "./primitives/triangle";
-import { lerp } from "./utils/lerp";
+import { Natlog } from "natural-log";
 
-const configs = {
-	// disables backdrop alpha, still allows rgba but is sped up
-	solidbg: false,
-	// desync canvas from event loops
-	desync: true,
-	// only draw wireframes
+import { Camera } from "./frame/camera";
+import { World } from "./frame/world";
+import { Mat } from "./maths/mat";
+import { Vec } from "./maths/vec";
+import { Mesh } from "./object/mesh";
+import { Model } from "./object/model";
+import { Canvas, RenderOptions } from "./render/canvas";
+
+new Natlog({ console: false });
+
+type Options = CanvasRenderingContext2DSettings &
+	RenderOptions & { modelCount: number; modelAlpha: number; modelWidth: number };
+
+const options: Required<Options> = {
+	alpha: true,
+	colorSpace: "srgb",
+	desynchronized: true,
+	willReadFrequently: false,
+
 	wireframe: false,
-	// disables sub-pixel rendering but causes alias issues
-	round: false,
-	// mouse x sensitivities (x180deg)
-	xsens: 1.5,
-	// mouse y sensitivities (x180deg)
-	ysens: 1,
-	// move distance
-	mvdist: 100,
-	// move accel decay rate
-	mvdecay: 0.97,
-	// number of cubes
-	cubecnt: 15,
-	// rgba alpha of cubes
-	cubealpha: 1,
+
+	modelCount: 1,
+	modelAlpha: 1,
+	modelWidth: 1000,
 };
 
-const cvsEle = <HTMLCanvasElement>document.getElementById("canvas3d");
-const main = () => {
-	const h = (cvsEle.height = window.innerHeight);
-	const w = (cvsEle.width = window.innerWidth);
+const world = new World();
+const camera = new Camera();
 
-	const cvs = new Cvs(cvsEle, configs);
-	const scene = new Scene();
-	const render = () => scene.render(cvs, configs);
+const canvas = new Canvas(document.getElementById("canvas") as HTMLCanvasElement, options);
+canvas.canvas.width = document.documentElement.clientWidth;
+canvas.canvas.height = document.documentElement.clientHeight;
 
-	type axis = `${0 | 1}${0 | 1}${0 | 1}`;
-	const getTri = (
-		x: axis,
-		y: axis,
-		z: axis,
-		rgb: `${number},${number},${number}`,
-	) => {
-		return new Tri(
-			<[Vec.vec3, Vec.vec3, Vec.vec3]>(
-				[x, y, z].map((axis) => axis.split("").map((v) => (+v ? w : 0)))
-			),
-			`rgba(${rgb},${configs.cubealpha})`,
-		);
-	};
+function render(): void {
+	canvas.render(world, camera, options);
+}
 
-	const tris: Tri[] = [
-		//top
-		getTri("001", "101", "011", "255,0,0"),
-		getTri("011", "101", "111", "255,0,0"),
+type GrayAxis = `${0 | 1}${0 | 1}${0 | 1}`;
 
-		//bottom
-		// getTri("000", "100", "110", "0,255,0"),
-		// getTri("000", "010", "110", "0,255,0"),
+function mesh(x: GrayAxis, y: GrayAxis, z: GrayAxis, color?: string): Mesh {
+	const vertices = [x, y, z].map((g) => {
+		return new Vec(g.split("").map((v) => +v * options.modelWidth));
+	});
 
-		//front
-		// getTri("000", "100", "101", "0,255,0"),
-		// getTri("000", "001", "101", "0,255,0"),
+	return new Mesh(vertices, color);
+}
 
-		//back
-		// getTri("010", "110", "011", "0,0,0"),
-		// getTri("110", "011", "111", "0,0,0"),
+const meshes = [
+	// top
+	mesh("010", "011", "111", "hsl(210, 100%, 50%)"),
+	mesh("010", "110", "111", "hsl(210, 100%, 50%)"),
 
-		//left
-		getTri("010", "000", "001", "0,0,255"),
-		getTri("010", "011", "001", "0,0,255"),
+	// bottom
+	mesh("000", "001", "101", "hsl(30, 100%, 50%)"),
+	mesh("000", "100", "101", "hsl(30, 100%, 50%)"),
 
-		//right
-		// getTri("100", "110", "111", "0,255,255"),
-		// getTri("100", "101", "111", "0,255,255"),
-	];
+	// front
+	mesh("001", "011", "111", "hsl(120, 100%, 50%)"),
+	mesh("001", "101", "111", "hsl(120, 100%, 50%)"),
 
-	// const ranPos = () => (Math.random() - 0.5) * configs.cubedisp * w;
+	// back
+	mesh("000", "010", "110", "hsl(270, 100%, 50%)"),
+	mesh("000", "100", "110", "hsl(270, 100%, 50%)"),
 
-	const cubes: Obj[] = [];
-	for (let i = 0; i < configs.cubecnt; i++) {
-		const n = i - configs.cubecnt / 2;
+	// left
+	mesh("000", "010", "011", "hsl(0, 100%, 50%)"),
+	mesh("000", "001", "011", "hsl(0, 100%, 50%)"),
 
-		const obj = new Obj(
-			tris,
-			// [ranPos(), ranPos(), ranPos()],
-			[n * w * 3, -Math.abs(n) * w, -w * 10],
-			[w / 2, w / 2, w / 2],
-		);
-		cubes.push(obj);
-		scene.putObj(obj);
-	}
+	// right
+	mesh("100", "110", "111", "hsl(180, 100%, 50%)"),
+	mesh("100", "101", "111", "hsl(180, 100%, 50%)"),
+];
 
-	const regVal: number[] = [];
-	const ax1Spd: number[] = [];
-	const ax2Spd: number[] = [];
-	for (let i = 0; i < configs.cubecnt; i++) {
-		regVal.push((Math.random() + 1) * 10);
-		ax1Spd.push(Math.random());
-		ax2Spd.push(Math.random());
-	}
+// const models: Model[] = [];
 
-	const rotcubes = (time: DOMHighResTimeStamp = 0) => {
-		for (let i = 0; i < cubes.length; i++) {
-			const tick = time / regVal[i];
-			cubes[i].setTransform(
-				Mat.rot([tick * ax1Spd[i], tick * ax2Spd[i], 0], {
-					deg: true,
-				}),
-			);
-		}
-		requestAnimationFrame(rotcubes);
-	};
-	rotcubes();
+// for (let i = 0; i < options.modelCount; i++) {
+// 	const n = i - options.modelCount / 2;
 
-	cvsEle.onmousemove = (ev) => {
-		scene.cam.setAxisRot(1, (ev.offsetX / w - 0.5) * 360 * configs.xsens);
-		scene.cam.setAxisRot(
-			2,
-			Math.min((ev.offsetY / h - 0.5) * 85 * configs.ysens, 85),
-		);
-	};
+// 	const model = new Model(
+// 		meshes,
+// 		new Vec([0.5, 0.5, 0.5]),
+// 		new Vec([n * 3, -Math.abs(n), -5]),
+// 	);
+// 	world.push(model);
+// 	models.push(model);
+// }
 
-	cvsEle.onkeydown = (ev) => {
-		const key = ev.key.toLowerCase();
+const model = new Model(
+	meshes,
 
-		switch (key) {
-			case "i": {
-				lerp(0.001 * Math.PI, 0.97, 0.001, (speed) => (scene.cam.fov -= speed));
-				return;
-			}
-			case "o": {
-				lerp(0.001 * Math.PI, 0.97, 0.001, (speed) => (scene.cam.fov += speed));
-				return;
-			}
-		}
+	Vec.fill(3, options.modelWidth / 2),
+	new Vec([0, 0, -2 * options.modelWidth]),
+);
+world.push(model);
 
-		const delta: Vec.vec3 = [0, 0, 0];
-		switch (key) {
-			case "w": {
-				delta[2] = configs.mvdist;
-				break;
-			}
-			case "s": {
-				delta[2] = -configs.mvdist;
-				break;
-			}
-			case "d": {
-				delta[0] = configs.mvdist;
-				break;
-			}
-			case "a": {
-				delta[0] = -configs.mvdist;
-				break;
-			}
-		}
-		lerp(
-			delta,
-			configs.mvdecay,
-			1,
-			(mv) => (scene.cam.pos = Vec.vSub(scene.cam.pos, mv)),
-			Vec.norm,
-			Vec.mul,
-		);
-	};
+// const rXv: number[] = [];
+// const rYv: number[] = [];
 
-	let fpsAcc = 0;
-	setInterval(() => {
-		console.clear();
-		console.log("fps", fpsAcc);
-		fpsAcc = 0;
-	}, 1000);
+// for (let i = 0; i < options.modelCount; i++) {
+// 	rXv.push(Math.random());
+// 	rYv.push(Math.random());
+// }
 
-	let update: FrameRequestCallback;
-	(update = () => {
-		render();
-		fpsAcc++;
-		requestAnimationFrame(update);
-	})();
+// function rotateModels(time: DOMHighResTimeStamp = 0): void {
+// 	for (let i = 0; i < options.modelCount; i++) {
+// 		const tick = time / 1000;
+
+// 		models[i].transform = Mat.rot(new Vec([tick * rXv[i], tick * rYv[i], 0]));
+// 	}
+
+// 	requestAnimationFrame(rotateModels);
+// }
+// rotateModels();
+
+// scene.cam.rot = [0, 0, Math.PI];
+
+canvas.canvas.onmousemove = (ev) => {
+	camera.rotation = new Vec([
+		(ev.offsetX / screen.width - 0.5) * 360 * 2,
+		(ev.offsetY / screen.height - 0.5) * 90,
+		0,
+	]);
 };
-window.addEventListener("resize", main);
-main();
+
+// canvasElement.onkeydown = (ev) => {
+// 	const key = ev.key.toLowerCase();
+
+// 	switch (key) {
+// 		case "i": {
+// 			lerp(0.001 * Math.PI, 0.97, 0.001, (speed) => (scene.cam.fov -= speed));
+// 			return;
+// 		}
+// 		case "o": {
+// 			lerp(0.001 * Math.PI, 0.97, 0.001, (speed) => (scene.cam.fov += speed));
+// 			return;
+// 		}
+// 	}
+
+// 	const delta: Vec.vec3 = [0, 0, 0];
+// 	switch (key) {
+// 		case "w": {
+// 			delta[0] = configs.mvdist;
+// 			break;
+// 		}
+// 		// case "s": {
+// 		// 	delta[2] = -configs.mvdist;
+// 		// 	break;
+// 		// }
+// 		// case "d": {
+// 		// 	delta[0] = configs.mvdist;
+// 		// 	break;
+// 		// }
+// 		// case "a": {
+// 		// 	delta[0] = -configs.mvdist;
+// 		// 	break;
+// 		// }
+// 	}
+// 	lerp(
+// 		delta,
+// 		configs.mvdecay,
+// 		1,
+// 		(mv) => {
+// 			// console.log(scene.cam.pos);
+// 			Mat.log(Mat.rot(scene.cam.rot));
+// 			scene.cam.pos = Vec.vSub(
+// 				scene.cam.pos,
+// 				Mat.transform(Mat.rot(scene.cam.rot), mv),
+// 			);
+// 		},
+// 		Vec.norm,
+// 		Vec.mul,
+// 	);
+// };
+
+// let fpsAcc = 0;
+// setInterval(() => {
+// 	console.clear();
+// 	console.log("fps", fpsAcc);
+// 	fpsAcc = 0;
+// }, 1000);
+
+// function main(): void {
+// 	render();
+// 	requestAnimationFrame(main);
+// }
+
+// main();
+
+render();
